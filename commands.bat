877f182aba9de6e8c04d7b3b666591c98f333b84
austin <austinlee199723@yahoo.com>
2021-08-26 01:03:33 -0700

    few css errors resolved

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
-cd amit-template1 && npm run build && cd .. && git add . && git commit -m "compressed one more image" && git push origin main
+cd amit-template1 && npm run build && cd .. && git add . && git commit -m "few css errors resolved" && git push origin main

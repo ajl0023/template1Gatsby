239a966f54c56650269a2bcba3ac24816f910dce
austin <austinlee199723@yahoo.com>
2021-08-26 01:02:24 -0700

    compressed one more image

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
-git add . && git commit -m "compressed one more image" && git push origin main
+cd amit-template1 && npm run build && cd .. && git add . && git commit -m "compressed one more image" && git push origin main

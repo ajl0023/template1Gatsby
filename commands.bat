bf89b768d9f0ae323905ab75f10ea26fc314af3f
austin <austinlee199723@yahoo.com>
2021-08-27 02:01:01 -0700

    finished site

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
-cd amit-template1 && npm run build && cd .. && git add . && git commit -m "responsiveness and bugs with scroll animation fixed" && git push origin main
+cd amit-template1 && npm run build && cd .. && git add . && git commit -m "finished site" && git push origin main

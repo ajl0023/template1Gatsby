3912ef7fc52424b6c02e7a1875dd0c106ab88edd
austin <austinlee199723@yahoo.com>
2021-08-27 14:45:20 -0700

    site should be mostly done now, edited video card on mobile devices

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
-cd amit-template1 && npm run build && cd .. && git add . && git commit -m "added responsive social bar" && git push origin main
+cd amit-template1 && npm run build && cd .. && git add . && git commit -m "site should be mostly done now, edited video card on mobile devices" && git push origin main

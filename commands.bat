ac566d3a674f0e3bcaac55a13145502fccbb9996
austin <austinlee199723@yahoo.com>
2021-08-27 17:46:49 -0700

    images are organized into a single directory

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
-cd amit-template1 && npm run build && cd .. && git add . && git commit -m "finished linting, organized images much better" && git push origin main
+cd amit-template1 && npm run build && cd .. && git add . && git commit -m "images are organized into a single directory" && git push origin main

232cb2c26168717e612606633712d4740b073cb0
austin <austinlee199723@yahoo.com>
2021-09-03 20:40:36 -0700

    added few new pages

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
-cd amit-template1 && npm run build && cd .. && git add . && git commit -m "have to fix some minor css issues" && git push origin main
+cd amit-template1 && npm run build && cd .. && git add . && git commit -m "added few new pages" && git push origin main
